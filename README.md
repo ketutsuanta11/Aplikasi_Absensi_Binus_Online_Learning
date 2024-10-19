6c5f8b699f07c70feae208da343cbb15844fd278
Ketut Suanta <ketut.suanta@lintasarta.co.id>
2024-10-19 20:39:07 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Absensi-Apps
+Project Aplikasi Absensi dengan Android Studio
+
+Nama: Ketut Suanta
+NIM: 2440089996
+Kelas: TECA
+
+
+
+Copyright (C) Ketut Suanta - 2024
+
+
+
+```
---
+++ app/src/main/java/com/azhar/absensi/viewmodel/HistoryViewModel.kt
@@ -0,0 +1,33 @@
+package com.azhar.absensi.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.azhar.absensi.database.DatabaseClient.Companion.getInstance
+import com.azhar.absensi.database.dao.DatabaseDao
+import com.azhar.absensi.model.ModelDatabase
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+/**
+ * Created by Ketut Suanta on 19-10-2024
+ */
+
+class HistoryViewModel(application: Application) : AndroidViewModel(application) {
+    var dataLaporan: LiveData<List<ModelDatabase>>
+    var databaseDao: DatabaseDao? = getInstance(application)?.appDatabase?.databaseDao()
+
+    fun deleteDataById(uid: Int) {
+        Completable.fromAction {
+            databaseDao?.deleteHistoryById(uid)
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+    }
+
+    init {
+        dataLaporan = databaseDao!!.getAllHistory()
+    }
+}
